bb1bda6cfde49099381c1182ed61640fc152889f
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2017-08-10 01:42:28 -0500

    improved markdown

--- index.rb
+++ index.rb
@@ -3,24 +3,26 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-set :port, 80
-set :bind, '0.0.0.0'
-set :public_folder, Proc.new { File.join(root, "public") }
+configure :production do
+  set :port, 80
+  set :bind, '0.0.0.0'
+  set :public_folder, Proc.new { File.join(root, "public") }
 
-get "/" do
-  erb :weather
-end
-
-post "/get_city" do
-  if params[:city]
-    content_type :json
-    RestClient.get('https://www.metaweather.com/api/location/search/?query=' + params[:city])
+  get "/" do
+    erb :weather
   end
-end
 
-post "/get_weather" do
-  if params[:city]
-    content_type :json
-    RestClient.get('https://www.metaweather.com/api/location/' + params[:city])
+  post "/get_city" do
+    if params[:city]
+      content_type :json
+      RestClient.get('https://www.metaweather.com/api/location/search/?query=' + params[:city])
+    end
+  end
+
+  post "/get_weather" do
+    if params[:city]
+      content_type :json
+      RestClient.get('https://www.metaweather.com/api/location/' + params[:city])
+    end
   end
 end
