746f000205674467f8dfc3a1c4b184909eee1cde
Seyedamirhossein Hesamian <hesamian@uwm.edu>
2017-08-10 07:50:53 -0500

    improved styling

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-require 'index'
+require './index'
 
 ## There is no need to set directories here anymore;
 ## Just run the application
--- index.rb
+++ index.rb
@@ -3,26 +3,24 @@ require 'sinatra'
 require 'rest-client'
 require 'json'
 
-configure :production do
-  set :port, 80
-  set :bind, '0.0.0.0'
-  set :public_folder, Proc.new { File.join(root, "public") }
+# set :port, 80
+# set :bind, '0.0.0.0'
+set :public_folder, Proc.new { File.join(root, "public") }
 
-  get "/" do
-    erb :weather
+get "/" do
+  erb :weather
+end
+
+post "/get_city" do
+  if params[:city]
+    content_type :json
+    RestClient.get('https://www.metaweather.com/api/location/search/?query=' + params[:city])
   end
+end
 
-  post "/get_city" do
-    if params[:city]
-      content_type :json
-      RestClient.get('https://www.metaweather.com/api/location/search/?query=' + params[:city])
-    end
-  end
-
-  post "/get_weather" do
-    if params[:city]
-      content_type :json
-      RestClient.get('https://www.metaweather.com/api/location/' + params[:city])
-    end
+post "/get_weather" do
+  if params[:city]
+    content_type :json
+    RestClient.get('https://www.metaweather.com/api/location/' + params[:city])
   end
 end
